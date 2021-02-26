48db8590884755bab33f7404648076b84b0da52b
Fasih-Innovation-Lab <79583545+Fasih-Innovation-Lab@users.noreply.github.com>
2021-02-26 15:39:06 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,34 @@ function compute()
     p = document.getElementById("principal").value;
     
 }
+        function compute()
+{
+    var principal = document.getElementById("principal").value;
+    var v = validate_principal();
+    if (v == 0)
+    return;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = (principal * years * rate)/100;
+    var amount = +principal + +interest;
+    var curr_year = new Date().getFullYear();
+    var result = document.getElementById("result");
+    result.innerHTML = "If you deposit " + "<mark>"+principal+",</mark><br>";
+    result.innerHTML += "at an interest rate of "+"<mark>"+rate+"%."+"</mark><br>"+"You will receive an amount of "+"<mark>"+amount+",</mark><br>"+"in the year "+"<mark>"+(+curr_year + +years)+"</mark><br>";
+    
+}
+function display_rate()
+{
+    var slider = document.getElementById("rate");
+    var output = document.getElementById("rate-op");
+    output.innerHTML = slider.value;
+}
+function validate_principal()
+{
+    var p = document.getElementById("principal").value;
+    if (p < 0) {
+        alert("Enter a positive number");
+        return 0;
+    } else return 1;
+}
         
